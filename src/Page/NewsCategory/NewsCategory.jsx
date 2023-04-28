a308a64fcc1ac768783e89afd960df06ab8f8475
himaloyash <himaloyashis98@gmail.com>
2023-04-28 22:01:01 +0600

    I'm undone

--- src/Page/NewsCategory/NewsCategory.jsx
+++ src/Page/NewsCategory/NewsCategory.jsx
@@ -1,5 +1,8 @@
+import moment from 'moment';
 import React from 'react';
-import { Button, Card } from 'react-bootstrap';
+import { Button, Card, Image } from 'react-bootstrap';
+import { FaRegBookmark, FaShareAlt } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const NewsCategory = ({ cat }) => {
     const { author, category_id, details, image_url, rating, thumbnail_url, title, total_view, _id, } = cat
@@ -7,14 +10,26 @@ const NewsCategory = ({ cat }) => {
 
 
     return (
-        <Card className="text-center">
-            <Card.Header>Featured</Card.Header>
+        <Card className="">
+            <Card.Header className='d-flex align-items-center'>
+                <Image style={{width:'40px'}} src={author?.img} roundedCircle />
+                <div className='ms-2 flex-grow-1'>
+                    <p className='mb-0'>{author?.name}</p>
+                    {moment(author?.published_date).format("dddd-MMMM-YYYY")}
+
+                </div>
+                <div className='d-flex'>
+                    <FaRegBookmark></FaRegBookmark>
+                    <FaShareAlt></FaShareAlt>
+                </div>
+            </Card.Header>
             <Card.Body>
-                <Card.Title>Special title treatment</Card.Title>
+                <Image className='w-100' src={image_url}></Image>
+                <Card.Title>{title}</Card.Title>
                 <Card.Text>
-                    With supporting text below as a natural lead-in to additional content.
+                    {details.length < 250 ? <>{details}</> : <>{details.slice(0, 250)} <Link to={`/news/${_id}`}>ReadMore...</Link></>}
                 </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <Link ><Button variant="primary">Se all news category</Button></Link>
             </Card.Body>
             <Card.Footer className="text-muted">2 days ago</Card.Footer>
         </Card>
